7c4d1e874db9ff83f5cd27ef9a6f539f4b0384c2
MaxDzioban <149308245+MaxDzioban@users.noreply.github.com>
2024-07-26 15:29:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,160 +1,94 @@
 import os
 import random
 import telebot
-
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
-
-@bot.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    bot.reply_to(message, "Hello, how are you doing?")
-
-# @bot.message_handler(func=lambda msg: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
-
-# games = {}
-# @bot.message_handler(func=lambda message: True)
-# def start_game(message):
-#     user_id = message.from_user.id
-    
-#     WORDS = [
-#         ["матір"],["браво"],["кавун"],
-#         ["місто"],["лікар"],["масло"],
-#         ["чайка"],["весна"],["осінь"],
-#         ["іврит"],["хатка"],["садок"],
-#         ["книга"],["фільм"],["пенал"],
-#         ["ручка"],["бабця"],["голос"],
-#         ["ліжко"],["думка"],["жінка"],
-#         ["гуска"],["качка"],["кишка"],
-#         ["ринок"],["павич"],["цукор"],
-#         ["щипці"],["фавор"],["циган"],
-#         ["олень"],["слина"],["спина"],
-#         ["дочка"],["обруч"],["школа"],
-#         ["театр"],["дрова"],["дідок"],
-#         ["мороз"],["гість"],["вечір"],
-#         ["канал"],["череп"],["пічка"],
-#         ["банда"],["зірка"],["камін"],
-#         ["філія"],["труси"],["зефір"],
-#         ["гуляш"],["банка"],["ґанок"],
-#         ["пацюк"],["груша"],["кабан"],
-#         ["метал"],["салат"],["чобіт"],
-#         ["їхати"],["нести"],["везти"],
-#         ["колія"],["геній"],["літак"],
-#         ["свиня"],["павук"],["індик"],
-#         ["пісня"],["клоун"],["лінія"],
-#         ["опера"],["бичок"],["вітер"],
-#         ["город"],["буряк"],["львів"],
-#         ["лодзь"],["сюжет"],["подія"],
-#         ["потік"],["битва"],["буква"],
-#         ["шахта"],["фірма"],["мітла"],
-#         ["смуга"],["казка"],["хмара"],
-#         ["увага"],["листя"],["думка"],
-#         ["тітка"],["війна"],["бійка"],
-#         ["група"],["кобра"],["жнива"],
-#         ["епоха"],["цифра"],["заєць"],
-#         ["дошка"],["ребро"],["кішка"]
-#     ]
-#     word = random.choice(WORDS)
-#     games[user_id] = {"word": word, "attempts": 6}
-#     bot.reply_to(message, "Гра почалась! Введи своє перше слово.")
-
-# def handle_message(message):
-#     user_id = message.from_user.id
-#     text = message.text.lower()
-
-#     if user_id in games:
-#         game = games[user_id]
-#         word = game["word"]
-
-#         if len(text) != len(word):
-#             bot.reply_to(message, f"Слово має бути з {len(word)} літер.")
-#             return
-
-#         if text == word:
-#             bot.reply_to(message, f"Вітаю! Ви вгадали слово '{word}'!")
-#             del games[user_id]
-#             return
-
-#         game["attempts"] -= 1
-#         if game["attempts"] == 0:
-#             bot.reply_to(message, f"Гру закінчено! Слово було '{word}'.")
-#             del games[user_id]
-#             return
-
-#         feedback = []
-#         for i in range(len(word)):
-#             if text[i] == word[i]:
-#                 feedback.append(f"{text[i]} (вірно)")
-#             elif text[i] in word:
-#                 feedback.append(f"{text[i]} (не на своєму місці)")
-#             else:
-#                 feedback.append(f"{text[i]} (немає в слові)")
-        
-#         bot.reply_to(message, " ".join(feedback))
-#         bot.reply_to(message, f"Залишилось спроб: {game['attempts']}")
-
-# bot.infinity_polling()
 games = {}
+set_of_words = set()
 
-WORDS = [
-    "матір", "браво", "кавун", "місто", "лікар", "масло", "чайка", "весна", "осінь", "іврит",
-    "хатка", "садок", "книга", "фільм", "пенал", "ручка", "бабця", "голос", "ліжко", "думка",
-    "жінка", "гуска", "качка", "кишка", "ринок", "павич", "цукор", "щипці", "фавор", "циган",
-    "олень", "слина", "спина", "дочка", "обруч", "школа", "театр", "дрова", "дідок", "мороз",
-    "гість", "вечір", "канал", "череп", "пічка", "банда", "зірка", "камін", "філія", "труси",
-    "зефір", "гуляш", "банка", "ґанок", "пацюк", "груша", "кабан", "метал", "салат", "чобіт",
-    "їхати", "нести", "везти", "колія", "геній", "літак", "свиня", "павук", "індик", "пісня",
-    "клоун", "лінія", "опера", "бичок", "вітер", "город", "буряк", "львів", "лодзь", "сюжет",
-    "подія", "потік", "битва", "буква", "шахта", "фірма", "мітла", "смуга", "казка", "хмара",
-    "увага", "листя", "думка", "тітка", "війна", "бійка", "група", "кобра", "жнива", "епоха",
-    "цифра", "заєць", "дошка", "ребро", "кішка"
-]
+with open("result_filtered.txt", "r", encoding="UTF-8") as file:
+    counter = 0
+    for line in file:
+        line = line.split(", ")
+        if "множина" in line[3]:
+            continue
+        else:
+            if len(line[1]) - 2 == 5:
+                counter += 1
+                set_of_words.add(line[1][1:-1])
+WORDS = list(set_of_words)
 
 @bot.message_handler(commands=['gamble'])
 def start_game(message):
     user_id = message.from_user.id
     word = random.choice(WORDS)
-    games[user_id] = {"word": word, "attempts": 6}
-    bot.reply_to(message, "Гра почалась! Введи своє перше слово.")
+    games[user_id] = {"word": word, "attempts": 6, "used_letters": {'correct': [], 'misplaced': [], 'absent': []}}
+    with open('photo_start.jpeg', 'rb') as photo:
+        bot.send_photo(message.chat.id, photo, caption="Привіт, це гра Словко, створена командою Ніч впродовж APPS Summercamp!\nТобі дано 6 спроб, щоб розкрити слово, яке складається з п'яти букв. Успіхів, гра починається!\nВведи своє перше слово:\n Щоб переглянути використані букви, натисни команду /abc")
+    games[user_id]["underline"] = ['_', '_', '_', '_', '_']
+    games[user_id]["existing_letters"] = []
+@bot.message_handler(commands=['abc'])
+def abc_ret(message):
+    user_id = message.from_user.id
+    if user_id in games:
+        used_letters = games[user_id]["used_letters"]
+        response = "Використані букви:\n"
+        response += "Вірно на місці: " + " ".join([f"__{letter}__" for letter in sorted(set(used_letters['correct']), key=lambda x: x[1])]) + "\n"
+        response += "Не на своєму місці: " + " ".join([f"*{letter}*" for letter in set(used_letters['misplaced'])])+ "\n"
+        response += "Немає в слові: " + " ".join([f"~{letter}~" for letter in set(used_letters['absent'])])
+        bot.reply_to(message, response, parse_mode='Markdown')
+    else:
+        bot.reply_to(message, "Ти не почав гру. Введи команду /gamble щоб почати.")
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     user_id = message.from_user.id
+    if user_id not in games:
+        return
+
     text = message.text.lower()
+    game = games[user_id]
+    word = game["word"]
+    if len(text) != len(word):
+        bot.reply_to(message, f"Слово має бути з {len(word)} літер.")
+        return
+    if text not in WORDS:
+        bot.reply_to(message, f"Я не знаю такого слова, спробуй ще раз.")
+        return 
 
-    if user_id in games:
-        game = games[user_id]
-        word = game["word"]
+    if text == word:
+        bot.reply_to(message, f"Вітаю! Ви вгадали слово '{word}'!")
+        del games[user_id]
+        return
 
-        if len(text) != len(word):
-            bot.reply_to(message, f"Слово має бути з {len(word)} літер.")
-            return
+    game["attempts"] -= 1
 
-        if text == word:
-            bot.reply_to(message, f"Вітаю! Ви вгадали слово '{word}'!")
-            del games[user_id]
-            return
+    feedback = generate_feedback(text, word, game["used_letters"], game["underline"], game["existing_letters"])
+    bot.reply_to(message, " ".join(feedback))
+    bot.reply_to(message, f"Залишилось спроб: {game['attempts']}")
+    
+    if game["attempts"] == 0:
+        bot.reply_to(message, f"Гру закінчено! Слово було '{word}'.")
+        del games[user_id]
 
-        game["attempts"] -= 1
-        if game["attempts"] == 0:
-            bot.reply_to(message, f"Гру закінчено! Слово було '{word}'.")
-            del games[user_id]
-            return
-
-        feedback = generate_feedback(text, word)
-        bot.reply_to(message, " ".join(feedback))
-        bot.reply_to(message, f"Залишилось спроб: {game['attempts']}")
-
-def generate_feedback(text, word):
+def generate_feedback(text, word, used_letters, underline, existing_letters):
     feedback = []
     for i in range(len(word)):
         if text[i] == word[i]:
-            feedback.append(f"{text[i]} (вірно)")
+            feedback.append(f"{text[i]} (вірно)\n")
+            if text[i] not in used_letters['correct']:
+                used_letters['correct'].append((text[i],i+1))
+            underline[i] = text[i]
         elif text[i] in word:
-            feedback.append(f"{text[i]} (не на своєму місці)")
+            feedback.append(f"{text[i]} (не на своєму місці)\n")
+            if text[i] not in used_letters['misplaced']:
+                used_letters['misplaced'].append(text[i])
+            if text[i] not in existing_letters:
+                existing_letters.append(text[i])
         else:
-            feedback.append(f"{text[i]} (немає в слові)")
+            feedback.append(f"{text[i]} (немає в слові)\n")
+            if text[i] not in used_letters['absent']:
+                used_letters['absent'].append(text[i])
     return feedback
 
 bot.infinity_polling()
