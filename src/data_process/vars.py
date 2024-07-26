11b25c6e4b0afc2850eb276feafe3702560fe220
Oleh Basystyi <basystyo@gmail.com>
2024-07-26 15:54:39 +0300

    merged wordle

--- src/data_process/vars.py
+++ src/data_process/vars.py
@@ -2,7 +2,13 @@
 Var module
 """
 
+import os
+import telebot
 from collections import defaultdict
 
 message_list = defaultdict(set)
 active_users = defaultdict(set)
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+bot = telebot.TeleBot(BOT_TOKEN)
+games = {}
+set_of_words = set()
